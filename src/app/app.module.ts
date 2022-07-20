a62c3e0ca6fc1ce02647bb81be4fba2150c0000f
DiegoFelipe7 <90659322+DiegoFelipe7@users.noreply.github.com>
2022-07-19 20:28:31 -0500

    Estructura fron end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
+//angular
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+//module
 import { AppRoutingModule } from './app-routing.module';
+//Components
 import { AppComponent } from './app.component';
+import { SingInComponent } from './auth/sing-in/sing-in.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SingInComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBnTvqtlb6Upt4SbE5M7xpBuhMW8lfk7uM",
+    authDomain: "juegodecartas-b0091.firebaseapp.com",
+    projectId: "juegodecartas-b0091",
+    storageBucket: "juegodecartas-b0091.appspot.com",
+    messagingSenderId: "659461209410",
+    appId: "1:659461209410:web:2b35e70a4dc89a2a3f9cdb",
+    measurementId: "G-VT5DRHZTY6"
+  }
 };
 
 /*
