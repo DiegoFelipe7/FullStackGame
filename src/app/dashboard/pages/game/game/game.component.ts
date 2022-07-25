7208315df832a181824263fb3ace4cbe7f4c6e4c
DiegoFelipe7 <90659322+DiegoFelipe7@users.noreply.github.com>
2022-07-25 15:35:57 -0500

    ajuste a crear juego

--- src/app/dashboard/pages/game/game/game.component.ts
+++ src/app/dashboard/pages/game/game/game.component.ts
@@ -11,8 +11,8 @@ import { GameService } from 'src/app/service/game.service';
 export class GameComponent implements OnInit {
   games: Game[] = [];
   player: userLogin = {
-    playerId: JSON.parse(localStorage.getItem("id")!),
-    email: JSON.parse(localStorage.getItem("email")!)
+    playerId: localStorage.getItem("id")!,
+    email: localStorage.getItem("email")!
   }
   constructor(private gameService: GameService, private router: Router) { }
 
@@ -26,8 +26,7 @@ export class GameComponent implements OnInit {
   }
 
   addPlayerGame(id: string): void {
-    console.log(this.player)
-    this.gameService.addPlayer(id, this.player).subscribe(() => this.router.navigate(["/Dashboard/GameBoard"]))
+    // this.gameService.addPlayer(id, this.player).subscribe(() => this.router.navigate(["/Dashboard/GameBoard"]))
   }
 
 }
